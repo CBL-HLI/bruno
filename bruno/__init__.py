cf41485ed6d48b0b31c941355fae24e8c22d8594
Amrit Singh <asingh.analytics@gmail.com>
2022-04-03 01:20:00 -0700

    fix init

--- bruno/__init__.py
+++ bruno/__init__.py
@@ -1,6 +1,32 @@
 """Top-level package for BRUNO."""
-import bruno.nn
+from types import ModuleType
+from importlib import import_module
 
 __author__ = """Amrit Singh"""
 __email__ = 'amrit.singh@hli.ubc.ca'
+__version__ = '0.1.0'
+
+
+class LazyLoader(ModuleType):
+    def __init__(self, local_name, parent_module_globals, name):
+        self._local_name = local_name
+        self._parent_module_globals = parent_module_globals
+        super().__init__(name)
+
+    def _load(self):
+        module = import_module(self.__name__)
+        self._parent_module_globals[self._local_name] = module
+        self.__dict__.update(module.__dict__)
+        return module
+
+    def __getattr__(self, item):
+        module = self._load()
+        return getattr(module, item)
+
+    def __dir__(self):
+        module = self._load()
+        return dir(module)
+
+nn = LazyLoader('nn', globals(), 'torch_geometric.nn')
+
 __version__ = '0.1.0'
