81f6cf121ff08b3c33f2f3f22c96d4c14aea9d4f
Amrit Singh <asingh.analytics@gmail.com>
2022-04-20 11:50:02 -0700

    update hyperparameters

--- bruno/learn/hyperparameters.py
+++ bruno/learn/hyperparameters.py
@@ -1,4 +1,3 @@
-
 class Hyperparameters():
     def __init__(self):
         self.num_node_features = None
@@ -6,6 +5,9 @@ class Hyperparameters():
         self.lr = 0.05
         self.w_decay = 5e-2   
         self.dropout = 0.3
-        self.epochs = 1000                
+        self.epochs = 1000
+        self.last_loss = 100
+        self.patience = 5
+        self.trigger_times = 0               
         self.cuda = True                
         self.device  =  None
